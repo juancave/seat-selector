5b2e4829579a42962d8361cd2e75b274c978850e
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-02-22 17:47:02 -0500

    Adding seat row component

---
+++ src/components/SeatRow/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './SeatRow.css';
+import { SeatsRow } from '../../types';
+import Seat from '../Seat';
+
+interface Props {
+  readonly data: SeatsRow;
+}
+
+const SeatRow: React.FC<Props> = (p: Props) => {
+  return (
+    <div className="seat-row-container">
+      <h2>{p.data.title}</h2>
+      <div className='seats-container'>
+        <div className='seats-section'>
+          {p.data.leftSeats.map((seat, index) => (<Seat key={index} seat={seat} />))}
+        </div>
+        <div className='seats-section'>
+          {p.data.rightSeats.map((seat, index) => (<Seat key={index} seat={seat} />))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SeatRow;
