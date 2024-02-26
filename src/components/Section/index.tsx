f0971b30de5a303b8fdd8b25b58c1f5f6e070755
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-02-26 11:05:39 -0500

    Applying tailwind to section component

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Section.css';
 import SeatRow from '../SeatRow';
 import { AirplaneData } from '../../types';
 import EmergencyExit from '../EmergencyExit';
@@ -10,9 +9,9 @@ interface Props {
 
 const Section: React.FC<Props> = (p: Props) => {
   return (
-    <div className="section-container">
-      <h1>{p.data.title}</h1>
-      <div className='section-seats'>
+    <div className="border-b border-gray-300 py-4">
+      <h1 className='m-0'>{p.data.title}</h1>
+      <div className='flex flex-col items-center'>
         {p.data.rows.map((row, index) => ('title' in row ? <SeatRow key={index} data={row} /> : <EmergencyExit key={index} data={row} />))}
       </div>
     </div>
