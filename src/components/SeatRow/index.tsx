6f3cca48cbb1506c16a72ad128a27290a4ecc0be
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-02-26 11:31:18 -0500

    Applying tailwind to seat row component

--- src/components/SeatRow/index.tsx
+++ src/components/SeatRow/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './SeatRow.css';
 import { SeatsRow } from '../../types';
 import Seat from '../Seat';
 
@@ -9,13 +8,13 @@ interface Props {
 
 const SeatRow: React.FC<Props> = (p: Props) => {
   return (
-    <div className="seat-row-container">
-      <h2>{p.data.title}</h2>
-      <div className='seats-container'>
-        <div className='seats-section'>
+    <div className="flex items-center gap-4">
+      <h2 className='w-5'>{p.data.title}</h2>
+      <div className='flex gap-12'>
+        <div className='flex items-center gap-4'>
           {p.data.leftSeats.map((seat, index) => (<Seat rowName={p.data.title} key={index} seat={seat} />))}
         </div>
-        <div className='seats-section'>
+        <div className='flex items-center gap-4'>
           {p.data.rightSeats.map((seat, index) => (<Seat rowName={p.data.title} key={index} seat={seat} />))}
         </div>
       </div>
