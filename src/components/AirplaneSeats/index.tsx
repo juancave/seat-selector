6ba7728994e7435f085bd7b80138b8be66d12349
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-03-01 14:44:38 -0500

    Updating bg color

--- src/components/AirplaneSeats/index.tsx
+++ src/components/AirplaneSeats/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AirplaneData } from '../../types';
+import { AirplaneData } from 'types';
 import Section from '../Section';
 
 interface Props {
@@ -12,7 +12,7 @@ const AirplaneSeats: React.FC<Props> = (p: Props) => {
     <div className="flex flex-col items-center">
       <div className='flex flex-col'>
         <h1 className='py-24 rounded-t-full font-bold text-4xl bg-gray-400 text-center'>{p.name}</h1>
-        <div className='border-x border-gray-300 px-4'>
+        <div className='border-x border-gray-300 px-4 bg-zinc-50'>
           {p.sections.map((section, index) => <Section key={index} data={section} />)}
         </div>
       </div>
