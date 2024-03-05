61190ab08c4721984339bcaebf206ac9c28a5309
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-03-05 15:08:26 -0500

    Updating airplane mobile styles

--- src/pages/Airplane/index.tsx
+++ src/pages/Airplane/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import AirplaneSeats from 'components/AirplaneSeats';
-import { airplaneSeatsDataMock, airplaneInformationDataMock, legendSeatsMockData } from 'data';
+import {
+  airplaneSeatsDataMock,
+  airplaneInformationDataMock,
+  legendSeatsMockData,
+} from 'data';
 import Cart from 'components/Cart';
 import AirplaneLegend from 'components/AirplaneLegend';
 import FlightInformation from 'components/FlightInformation';
@@ -16,33 +20,46 @@ const AirplanePage: React.FC = () => {
     setSeatsLimit(airplaneInformationDataMock.reservation.seats);
     setDefaultSeats(airplaneInformationDataMock.reservation.defaultSeats);
 
-    const seats = airplaneSeatsDataMock.reduce((seats: Seat[], currentSeat: AirplaneData) => {
-      const seatsPerRow = currentSeat.rows.reduce((seatsPerRow: Seat[], currentSeatRow: SeatsRow) => {
-        seatsPerRow = [...seatsPerRow, ...currentSeatRow.seats];
+    const seats = airplaneSeatsDataMock.reduce(
+      (seats: Seat[], currentSeat: AirplaneData) => {
+        const seatsPerRow = currentSeat.rows.reduce(
+          (seatsPerRow: Seat[], currentSeatRow: SeatsRow) => {
+            seatsPerRow = [...seatsPerRow, ...currentSeatRow.seats];
 
-        return seatsPerRow;
-      }, [] as Seat[]);
+            return seatsPerRow;
+          },
+          [] as Seat[],
+        );
 
-      return [...seats, ...seatsPerRow];
-    }, [] as Seat[]);
+        return [...seats, ...seatsPerRow];
+      },
+      [] as Seat[],
+    );
 
-    airplaneInformationDataMock.reservation.defaultSeats.forEach((defaultSeat: string) => {
-      const selectedSeat = seats.find((seat) => `${seat.row}${seat.name}` === defaultSeat);
+    airplaneInformationDataMock.reservation.defaultSeats.forEach(
+      (defaultSeat: string) => {
+        const selectedSeat = seats.find(
+          (seat) => `${seat.row}${seat.name}` === defaultSeat,
+        );
 
-      if (selectedSeat) {
-        addSeat({
-          ...selectedSeat,
-          selectedByDefault: true,
-        });
-      }
-    });
+        if (selectedSeat) {
+          addSeat({
+            ...selectedSeat,
+            selectedByDefault: true,
+          });
+        }
+      },
+    );
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div className="flex md:flex-row md:items-start flex-col items-center justify-center gap-10 mt-10 mb-10">  
-      <AirplaneSeats data={airplaneInformationDataMock} sections={airplaneSeatsDataMock} />
-      <div className='flex flex-col gap-10'>
-        <div className='py-28 hidden md:block'></div>
+    <div className="flex md:flex-row md:items-start flex-col items-center justify-center gap-10 mt-10 mb-10">
+      <AirplaneSeats
+        data={airplaneInformationDataMock}
+        sections={airplaneSeatsDataMock}
+      />
+      <div className="flex flex-row gap-10 justify-center md:flex-col flex-wrap md:flex-nowrap md:justify-normal">
+        <div className="py-28 hidden md:block"></div>
         <AirplaneLegend data={legendSeatsMockData} />
         <FlightInformation data={airplaneInformationDataMock} />
         <Cart />
