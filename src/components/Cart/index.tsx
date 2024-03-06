f6758cdb3cf48888d9a79cd32ef0fa5123fc508c
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-03-06 16:11:24 -0500

    Updating cart detail

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useCartStore from 'store/cart';
+import { SeatLocationName, SeatTypeName } from 'types';
 
 const Cart: React.FC = () => {
   const seats = useCartStore((state) => state.seats);
@@ -12,21 +13,37 @@ const Cart: React.FC = () => {
       <h2 className="border-b-4 pb-2 font-bold">Selected seats:</h2>
       <div className="flex flex-col gap-1">
         {seats.map((seat, index) => (
-          <div key={index} className="flex justify-between">
-            <span className="flex gap-1">
-              <span className="font-bold">
-                {seat.row}
-                {seat.name}
-              </span>
-              <span>-</span>
-              {seat.selectedByDefault ? (
-                <span>$0 - Included</span>
-              ) : (
-                <span>${seat.price}</span>
-              )}
-            </span>
+          <div
+            key={index}
+            className="text-sm flex justify-between items-center border p-1 rounded-md shadow-[0_1px_5px_rgb(0,0,0,0.1)]"
+          >
+            <div className="flex justify-around flex-col">
+              <div className="flex gap-1">
+                <span className="font-bold">Seat:</span>
+                <span>
+                  {seat.row}
+                  {seat.name}
+                </span>
+              </div>
+              <div className="flex gap-1">
+                <span className="font-bold">Section:</span>
+                <span>{SeatTypeName[seat.type]}</span>
+              </div>
+              <div className="flex gap-1">
+                <span className="font-bold">Location:</span>
+                <span>{SeatLocationName[seat.location]}</span>
+              </div>
+              <div className="flex gap-1">
+                <span className="font-bold">Price:</span>
+                {seat.selectedByDefault ? (
+                  <span>$0 - Included</span>
+                ) : (
+                  <span>${seat.price}</span>
+                )}
+              </div>
+            </div>
             <button
-              className="bg-white hover:bg-gray-100 text-gray-800 font-semibold px-1 py-0 border border-gray-400 rounded shadow"
+              className="bg-white hover:bg-gray-100 text-gray-800 font-semibold px-1 py-0 border border-gray-400 rounded shadow h-8 w-8"
               onClick={() => removeSeat(seat)}
             >
               X
