602aac6f347e0b1b54a88559e1dea00a78bd6275
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-03-06 14:09:46 -0500

    Passing down the context

--- src/components/AirplaneSeats/index.tsx
+++ src/components/AirplaneSeats/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AirplaneData, FlightInformation } from 'types';
-import Section from '../Section';
+import Section from 'components/Section';
+import { ContextType, SeatsContext } from 'context';
 
 interface Props {
   readonly data: FlightInformation;
@@ -9,16 +10,18 @@ interface Props {
 
 const AirplaneSeats: React.FC<Props> = (p: Props) => {
   return (
-    <div className="flex flex-row items-center">
-      <div className="flex flex-col">
-        <div className="py-32 rounded-t-full bg-zinc-50 border"></div>
-        <div className="border-x border-gray-300 px-4 bg-zinc-50 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-          {p.sections.map((section, index) => (
-            <Section key={index} data={section} />
-          ))}
+    <SeatsContext.Provider value={ContextType.AIRPLANE}>
+      <div className="flex flex-row items-center">
+        <div className="flex flex-col">
+          <div className="py-32 rounded-t-full bg-zinc-50 border"></div>
+          <div className="border-x border-gray-300 px-4 bg-zinc-50 shadow-[0_3px_10px_rgb(0,0,0,0.2)] pb-4">
+            {p.sections.map((section, index) => (
+              <Section key={index} data={section} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </SeatsContext.Provider>
   );
 };
 
